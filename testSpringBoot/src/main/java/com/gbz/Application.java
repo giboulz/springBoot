4b9ba1e280f0bb695e66b81dde18d6ddfaed0e54
Cedric GIBOULOT <cedric.giboulot@bnpparibas.com>
2016-11-04 16:37:59 +0100

    adding configuration for war deployement

--- testSpringBoot/src/main/java/com/gbz/Application.java
+++ testSpringBoot/src/main/java/com/gbz/Application.java
@@ -2,16 +2,26 @@ package com.gbz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-
-
+	/**
+	 * use for jar
+	 * 
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 	}
 
-
+	/**
+	 * Used when run as WAR
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 
 }
